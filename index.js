7becadab1fc2078aab40e9974a0c7bd8bb70be02
verne1997 <sourjya@refactor.academy>
2023-06-12 17:13:59 +0530

    added basic http routes

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World")
+const express = require("express");
+const app = express(); // app is basically a Express application
+const port = 5000;
+ //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
+
+app.get("/api/:id",(req,resp)=>{  
+    console.log("Request params : ",req.params['id'])    
+    console.log("Control is here : GET")
+    resp.send(`Test Response Data : ${req.params['id']}`)
+})
+
+app.post("/api",(req,resp)=>{    //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
+    console.log("Control is here: POST")
+})
+
+app.put("/api",(req,resp)=>{    //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
+    console.log("Control is here: PUT")
+})
+
+app.delete("/api",(req,resp)=>{    //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
+    console.log("Control is here: DELETE")
+})
+
+app.listen(port,()=>{
+    console.log(`Server listening on port : ${port} `)
+}) 
