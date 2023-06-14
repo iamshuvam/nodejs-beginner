0bebf53c728ad236169126353bf5b72727cd89aa
verne1997 <sourjya@refactor.academy>
2023-06-14 16:52:47 +0530

    second commit

--- index.js
+++ index.js
@@ -2,16 +2,48 @@ const express = require("express");
 const app = express(); // app is basically a Express application
 const port = 5000;
 const mongoose = require("mongoose");
- //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
+const User = require("./src/models/userSchema");
 
-app.get("/api/:id",(req,resp)=>{  
+// middleware
+app.use(express.json())
+// app.use((req,resp,next)=>{
+//     console.log("Inside middleware : ",req.headers["content-type"] )
+//    if(req.headers["content-type"] == "application/json") 
+//       next()
+//    else
+//       resp.send("Bad Request")
+// })
+ //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
+ // MVC - > ( MODEL VIEW CONTROLLER)
+app.get("/api/:id",async (req,resp)=>{  
+   try{
     console.log("Request params : ",req.params['id'])    
     console.log("Control is here : GET")
-    resp.send(`Test Response Data : ${req.params['id']}`)
+    let fetchedUser = await User.findById(req.params.id)
+
+    resp.json(fetchedUser)
+   }
+   catch(err){
+    console.log(err)
+    resp.send("Bad Request")
+   }
+    
 })
 
-app.post("/api",(req,resp)=>{    //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
-    console.log("Control is here: POST")
+app.post("/api/user", async (req,resp)=>{    //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
+    try{
+        console.log("Control is here: POST")
+        console.log("Request body ",req.body)
+        let user = new User(req.body)
+        let savedUser = await user.save();
+        console.log("User added successfully")
+        resp.json(savedUser)
+
+    }
+    catch(err){
+        console.log(err)
+    }                                  
+    
 })
 
 app.put("/api",(req,resp)=>{    //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
@@ -19,7 +51,7 @@ app.put("/api",(req,resp)=>{    //    http operations : adding new data ( POST )
 })
 
 app.delete("/api",(req,resp)=>{    //    http operations : adding new data ( POST ), fetching data ( GET ), update some data ( PUT ) , delete data ( DELETE )
-    console.log("Control is here: DELETE")
+    console.log("Control is here: DELETE") 
 })
 
 const db_url = "mongodb+srv://Shuvam123:hHUoznjJzfgg2J7h@practicecluster.kalpnxs.mongodb.net/?retryWrites=true&w=majority"
@@ -44,7 +76,7 @@ async function connectDb(){
             
         
 
-app.listen(port,()=>{
-    connectDb()
+app.listen(port,async ()=>{
+    await connectDb()
     console.log(`Server listening on port : ${port} `)
 }) 
